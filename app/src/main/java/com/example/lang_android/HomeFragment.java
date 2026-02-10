c74cd75074c5129922a28d262967e0b84de248e3
Involuntar <ilya.nevolin.2004@mail.ru>
2026-02-10 23:18:30 +0500

    сделал фильтрацию по типу

--- app/src/main/java/com/example/lang_android/HomeFragment.java
+++ app/src/main/java/com/example/lang_android/HomeFragment.java
@@ -9,10 +9,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ScrollView;
 
 public class HomeFragment extends Fragment {
     CardView quarksCard, ramenCard, pieCard;
+    Button breakfastBtn, lunchBtn, dinnerBtn, allBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -21,6 +23,11 @@ public class HomeFragment extends Fragment {
         quarksCard = view.findViewById(R.id.quark_pancakes_card);
         ramenCard = view.findViewById(R.id.ramen_card);
         pieCard = view.findViewById(R.id.shepherds_pie_card);
+
+        breakfastBtn = view.findViewById(R.id.breakfast_btn);
+        lunchBtn = view.findViewById(R.id.lunch_btn);
+        dinnerBtn = view.findViewById(R.id.dinner_btn);
+        allBtn = view.findViewById(R.id.all_btn);
 
         Fragment quarkFragment = new QuarkFragment();
         Fragment ramenFragment = new RamenFragment();
@@ -44,6 +51,42 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 setCurrentFragment(pieFragment);
+            }
+        });
+
+        breakfastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quarksCard.setVisibility(View.VISIBLE);
+                ramenCard.setVisibility(View.GONE);
+                pieCard.setVisibility(View.GONE);
+            }
+        });
+
+        lunchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quarksCard.setVisibility(View.GONE);
+                ramenCard.setVisibility(View.VISIBLE);
+                pieCard.setVisibility(View.GONE);
+            }
+        });
+
+        dinnerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quarksCard.setVisibility(View.GONE);
+                ramenCard.setVisibility(View.GONE);
+                pieCard.setVisibility(View.VISIBLE);
+            }
+        });
+
+        allBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quarksCard.setVisibility(View.VISIBLE);
+                ramenCard.setVisibility(View.VISIBLE);
+                pieCard.setVisibility(View.VISIBLE);
             }
         });
 
